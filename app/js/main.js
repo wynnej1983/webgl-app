fff894a59fa8a0a5f0bc51d2eee4c2b934a2223c
Jonathan Wynne <wynnej1983@gmail.com>
2014-06-21 15:18:23 +0900

    add tests and some code cleanup

--- app/js/main.js
+++ app/js/main.js
@@ -1,44 +1,43 @@
 (function(window) {
 'use strict';
 
-
 //Main app class
 function App() {
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-    this.camera.position.x = 500;
-    this.camera.position.y = 500;
     this.camera.position.z = 1000;
     this.scene = new THREE.Scene();
 
     //create 9 images and add to scene
-    for(var i = 0; i < 3; i++) {
-      for(var j = 0; j < 3; j++) {
+    for(var row = -1; row < 2; row++) {
+      for(var col = -1; col < 2; col++) {
         var img = this.createImage();
-        img.position.x = (i * 500);
-        img.position.y = (j * 500);
+        img.position.x = col * 500;
+        img.position.y = row * 500;
 
         this.scene.add(img);
       }
     }
 
-    //set camera to look at central image
-    this.camera.lookAt(this.scene.children[4].position);
+    //set camera to look at central of scene
+    this.camera.lookAt(this.scene.position);
 
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(window.innerWidth, window.innerHeight);
 
-    document.body.appendChild(this.renderer.domElement);
+    document.querySelector('#test-container').appendChild(this.renderer.domElement);
     //add slider to body
-    document.body.appendChild(this.createSlider());
+    this.slider = this.createSlider();
+    document.body.appendChild(this.slider);
 
     //rerender scene on window resize
     window.addEventListener('resize', function() {
-      var width = window.innerWidth,
-          height = window.innerHeight;
-      this.renderer.setSize(width, height);
-      this.camera.aspect = width / height;
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
       this.camera.updateProjectionMatrix();
-    });
+      this.camera.aspect = window.innerWidth / window.innerHeight;
+      this.slider.style.left = (window.innerWidth/2) - (100) + 'px';
+      this.slider.style.bottom = '10px';
+      this.renderer.render(this.scene, this.camera);
+    }.bind(this));
 
     //render scene
     this.renderer.render(this.scene, this.camera);
@@ -80,7 +79,6 @@ App.prototype.createSlider = function() {
 };
 
 //create app
-window.App = App;
-new App();
+window.App = new App();
 
 })(window);
--- app/test/main.spec.js
+++ app/test/main.spec.js
@@ -1,6 +1,21 @@
+'use strict';
+
 describe('App', function() {
-    it('should create scene with 9 images', function() {
-      var app = new window.App();
+    var app = window.App;
+  
+    it('should create scene with 9 images', function(done) {
       expect(app.scene.children).to.be.ok();
+      done();
     });
+
+    it.skip('should scale on slider change', function(done) {
+      var slider = app.slider;
+      slider.onchange(function() {
+        expect(app.camera.fov).to.be.greaterThan(75);
+        done();
+      });
+      //force slider change
+      slider.value = 100;
+    });
+    
   });
